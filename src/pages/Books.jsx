6e9efbbe8b7bb37a1fe837ca4ced8f50a03db0bc
Mohammad Faiz Ahanger <Mohammadfaiz_2019bcse002@nitsri.net>
2022-10-20 20:29:54 +0530

    fetch all books from db and use map function to display them in div elements

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,8 +1,40 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from 'axios'
 
 
 const Books = () => {
+
+    const [books, setBooks] = useState([])
+
+    useEffect(() => {
+        const fetchAllBooks = async () => {
+            try {
+                const res = await axios.get("http://localhost:8800/books")
+                setBooks(res.data)
+            } catch(err) {
+                console.log(err)
+            }
+        }
+        fetchAllBooks()
+    },[])
+
+
     return (
-        <div>Books</div>
+        <div>
+            <h1>BookShop</h1>
+            <div className="books">
+                {books.map((book) => (
+                    <div className="book" key={book.id}>
+                        {book.cover && <img src={book.cover} alt="" />}
+                        <h2>{book.title}</h2>
+                        <p>{book.desc}</p>
+                        <span>${book.price}</span>
+                    </div>
+                ))}
+            </div>
+            
+        </div>
     )
 }
 
